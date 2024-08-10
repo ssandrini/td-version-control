31779657637c7b65257410c033e5a6946fc4b16d
BraveJero <jbrave271@gmail.com>
2024-08-10 15:53:15 -0300

    high value work

--- mariana/electron/version-mgr/index.ts
+++ mariana/electron/version-mgr/index.ts
@@ -3,7 +3,7 @@ import simpleGit from 'simple-git';
 
 export const listVersions = async () => {
     try {
-        const tdDir = path.join(process.cwd(), '.td');
+        const tdDir = path.join('/home/jbrave/Desktop/ITBA/PF/td1/', '.td');
         const git = simpleGit(tdDir);
         const tags = await git.tags();
         if (tags.all.length === 0) {

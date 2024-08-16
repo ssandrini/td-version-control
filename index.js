b8f1f5b8b8d9bbf64dad5f711e986bf8730e3654
Juan García <juanigarcia@itba.edu.ar>
2024-08-16 00:27:39 -0300

    Add detection for new nodes

--- index.js
+++ index.js
@@ -58,43 +58,7 @@ const initRepo = async () => {
 
 const newVersion = async (name, message) => {
     try {
-        const toeFile = findToeFile();
-        if (!toeFile) {
-            console.log('No .toe file found');
-            return;
-        }
-
-        try {
-            execSync(`toeexpand.exe ${toeFile}`, { stdio: 'inherit' });
-            console.log(`Expanded ${toeFile}`);
-        } catch (e) {
-            // por ahora nada con el error
-        }
-
-        const tocFilePath = `${toeFile}.toc`;
-        const tocContent = fs.readFileSync(tocFilePath, 'utf-8');
-        const fileList = tocContent.split('\n').map(line => line.trim()).filter(line => line !== '');
-
-        // Mover archivos a .td
-        const tdDir = path.join(process.cwd(), '.td');
-        if (!fs.existsSync(tdDir)) {
-            fs.mkdirSync(tdDir);
-        }
-
-        for (const file of fileList) {
-            const sourcePath = path.join(process.cwd(), `${toeFile}.dir`, file);
-            const targetPath = path.join(tdDir, `${toeFile}.dir`, file);
-            //console.log(`moving file: ${file} from ${sourcePath} to ${targetPath}`);
-            fs.renameSync(sourcePath, targetPath);
-        }
-
-        const tocTargetPath = path.join(tdDir, path.basename(tocFilePath));
-        fs.renameSync(tocFilePath, tocTargetPath);
-
-        const toeDirPath = path.join(process.cwd(), `${toeFile}.dir`);
-        fs.rm(toeDirPath, { recursive: true, force: true });
-
-        process.chdir(tdDir);
+        expandAndMove();
         const git = simpleGit();
         await git.add('.');
         await git.commit(`-m "${message}" --allow-empty`);
@@ -183,8 +147,105 @@ const currentVersion = async () => {
 
 const findToeFile = () => {
     const files = fs.readdirSync('.');
-    return files.find(file => file.endsWith('.toe'));
+    // Solo me quedo con el archivo que no tiene el numero de version
+    const toeFileRegex = /^[^.]+\.toe$/;
+    return files.find(file => toeFileRegex.test(file));
 };
+
+const expandAndMove = () => {
+    const toeFile = findToeFile();
+    if (!toeFile) {
+        console.log('No .toe file found');
+        return;
+    }
+    try {
+        execSync(`toeexpand.exe ${toeFile}`, { stdio: 'inherit' });
+        console.log(`Expanded ${toeFile}`);
+    } catch (e) {
+        // por ahora nada con el error
+    }
+
+    const tocFilePath = `${toeFile}.toc`;
+    const tocContent = fs.readFileSync(tocFilePath, 'utf-8');
+    const fileList = tocContent.split('\n').map(line => line.trim()).filter(line => line !== '');
+
+    // Mover archivos a .td
+    const tdDir = path.join(process.cwd(), '.td');
+    if (!fs.existsSync(tdDir)) {
+        fs.mkdirSync(tdDir);
+    }
+
+    for (const file of fileList) {
+        const sourcePath = path.join(process.cwd(), `${toeFile}.dir`, file);
+        const targetPath = path.join(tdDir, `${toeFile}.dir`, file);
+        //console.log(`moving file: ${file} from ${sourcePath} to ${targetPath}`);
+        fs.renameSync(sourcePath, targetPath);
+    }
+
+    const tocTargetPath = path.join(tdDir, path.basename(tocFilePath));
+    fs.renameSync(tocFilePath, tocTargetPath);
+
+    const toeDirPath = path.join(process.cwd(), `${toeFile}.dir`);
+    fs.rm(toeDirPath, { recursive: true, force: true });
+
+    process.chdir(tdDir);
+}
+
+const nodeChanges = async () => {
+    try {
+        const tocFile = `${findToeFile()}.toc`;
+        expandAndMove();
+        const git = simpleGit();
+        process.chdir('..');
+        const diffLines = (await git.diff([tocFile])).split('\n');
+        const addedLines = diffLines.filter(line => {
+            return line.startsWith('+') && !line.startsWith('+++');
+        });
+        const deltedLines = diffLines.filter(line => {
+            return line.startsWith('-') && !line.startsWith('---');
+        });
+      
+        const addedNodes = new Set(addedLines.map(line => {
+            return extractNodeName(line);
+        }));
+
+        const deletedNodes = new Set(deltedLines.map(line => {
+            return extractNodeName(line);
+        }));
+
+        for (const node of addedNodes) {
+            console.log(`Added node ${node} (${await getNodeInfo(node)})`);
+        }
+
+        for (const node of deletedNodes) {
+            console.log(`Deleted node ${node}`);
+        }
+
+    } catch (error) {
+        console.error('Error: ', error);
+    } finally {
+        process.chdir('..');
+    }
+}
+
+const extractNodeName = (diffLine) => {
+    const lineContent = diffLine.slice(1).trim();
+    const parts = lineContent.split('/');
+    const fileNameWithExtension = parts[parts.length - 1];
+    const fileName = fileNameWithExtension.split('.')[0];
+    return fileName;
+}
+
+const getNodeInfo = async (node) => {
+    try {
+        const toeDirPath = path.join(process.cwd(), '.td',  `${findToeFile()}.dir`, 'project1', `${node}.n`);
+        const fileContent = await fs.readFile(toeDirPath, 'utf-8');
+        const firstLine = fileContent.split('\n')[0];
+        return firstLine;
+    } catch (error) {
+        console.error('Error reading file:', error);
+    }
+}
 
 const findExpandedDir = () => {
     const dirs = fs.readdirSync('.').filter(file => fs.lstatSync(file).isDirectory());
@@ -220,6 +281,7 @@ yargs(hideBin(process.argv))
     })
     .command('list-versions', 'List all available versions', () => {}, listVersions)
     .command('current-version', 'Show the current version', () => {}, currentVersion)
+    .command('nodes', 'List diff between versions', () => {}, nodeChanges)
     .demandCommand(1, 'You need at least one command before moving on')
     .help()
     .argv;
